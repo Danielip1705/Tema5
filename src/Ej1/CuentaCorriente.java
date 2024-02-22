11dfd6046a104a4f3a6ed92dee9c7f051475dee0
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-02-22 12:47:05 +0100

    22/2/24

---
+++ src/Ej1/CuentaCorriente.java
@@ -0,0 +1,33 @@
+package Ej1;
+
+public class CuentaCorriente {
+	private int dni;
+	private String nombre;
+	private double saldo;
+	
+	
+	
+	
+	
+	
+	
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+}
